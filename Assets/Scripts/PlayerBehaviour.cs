15adb33589d639b82dd475b8e42312de69bc3858
Ramon Felipe da Cruz Ferreira <ramon_felipe@outlook.com>
2016-09-06 01:45:42 -0300

    Implementei a movimentação via joystick no mesmo script. Comentei acho que todos os códigos para que a gente possa fazer manutenção mais fácil.
    O jogador se move pelo joystick quando o OnMobile estiver marcado.
    
    Botão branco grande serve para correr
    
    chamada da função ChangeSpeed está comentada, se precisar pode descomentar.
    
    Precisamos ver se vamos utilizar as variaveis de sneaking e running porque o jogador pode entrar em modo sneaking apenas movimentando mais devagar o input.
    
    O botão branco grande serviria apenas para run, enquanto ele está pressionado a velocidade do jogador é multiplicado pelo valor de runMultiplier.
    
    Dessa forma precisamos de apenas um botão do lado direito.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,19 +4,24 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-    public bool onMobile;
+    [Header("Testando no Celular?")]
+    public bool onMobile; //para saber se estamos no mobile, controle manual
 
-    public float walkingSpeed;
-    public float runningSpeed;
-    public float sneakingSpeed;
+    [Header("Velocidades")]
+    public float runMultiplier = 2; //multiplicador de velocidade quando estiver correndo
+    public float walkingSpeed = 5; //velocidade padrao de movimento
+    public float runningSpeed = 10; //velocidade ao correr
+    public float sneakingSpeed = 4; //velocidade de sneaking
 
-    public float actualSpeed;
+    [Header("Velocidade Final")]
+    public float actualSpeed; //velocidade atual
 
-    public bool running;
-    public bool sneaking;
-    public bool walking;
+    [Header("Estados do Player")]
+    public bool running; //controle para saber se esta correndo ou não
+    public bool sneaking; //controle para saber se esta em modo sneaking ou não
+    public bool walking; //controle para sabe se esta andando ou ano
 
-    private float directionX;
+    private float directionX; //valor do input do jogador
     
 
 	// Use this for initialization
@@ -28,28 +33,54 @@ public class PlayerBehaviour : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (!onMobile) { 
-            directionX = Input.GetAxis("Horizontal");
+	void FixedUpdate () {
+
+        // Condição para verificar se estamos jogando no pc ou no celular
+        //Caso estejamos no pc o jogo recebe input do teclado
+        //se estiver no mobile, receberá input do joystick na tela
+        if (!onMobile) {
+            directionX = Input.GetAxis("Horizontal"); //input do teclado
+        } else
+        {
+            directionX = CrossPlatformInputManager.GetAxis("Horizontal"); //input do joystick
         }
         
+        //Código para pegar quando o jogador apertar shift esquerdo para correr
         if(Input.GetKeyDown(KeyCode.LeftShift))
         {
-            ToggleRun();
+            ToggleRun(); //função de correr
         }
+
+        //Código para pegar quando o jogador apertar control esquerdo para
+        //entrar no modo sneaking
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            ToggleSneaking();
+            ToggleSneaking(); //função para sneakar
         }
 
-        actualSpeed = walkingSpeed;
-        walking = true;
 
-        changeSpeed();
+        //velocidade utilizada na movimentação recebe a 
+        //velocidade de andar por padrao
+        actualSpeed = walkingSpeed; 
+        walking = true; //jogador sempre está andando?
 
+        //Para utilizar os botoões de trigger descomenta a linha abaixo
+        //changeSpeed(); MOdifica a velocidade
+
+        //Enquanto o jogador pressionar o botão de correr
+        //o jogador terá sua velocidade multiplicada por "runMultiplier"
+        if (CrossPlatformInputManager.GetButton("Run"))
+        {
+            actualSpeed *= runMultiplier;
+        }
+
+        //linha que movimento o jogador
         transform.Translate(new Vector3(directionX * actualSpeed * Time.deltaTime, 0f, 0f));
 	}
 
+    /// <summary>
+    /// Função que modifica a velocidade de acordo com o toggle que esta ativo
+    /// </summary>
     void changeSpeed()
     {
         if(running)
@@ -68,11 +99,9 @@ public class PlayerBehaviour : MonoBehaviour {
 
     }
 
-    public void SetDirectionX(float direction)
-    {
-        directionX = direction;
-    }
-
+    /// <summary>
+    /// Ativa o modo de correr
+    /// </summary>
     public void ToggleRun()
     {
         running = !running;
@@ -80,12 +109,20 @@ public class PlayerBehaviour : MonoBehaviour {
         sneaking = false;
 
     }
+
+    /// <summary>
+    /// Ativa modo de andar
+    /// </summary>
     public void ToggleWalking()
     {
         walking = !walking;
         running = false;
         sneaking = false;
     }
+
+    /// <summary>
+    /// Ativa modo sneaking
+    /// </summary>
     public void ToggleSneaking()
     {
         sneaking = !sneaking;
