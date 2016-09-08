31b147dbe97a68da22523194f140b3ff45d119ee
Jonnathan <jonnathan013@gmail.com>
2016-09-08 16:42:16 -0300

    -Mostra descrição de objeto ao clicar nele(deve estar a uma distancia minima estabelecida no script do objeto)-     Se a descrição estiver aberta e o jogador se distancia do objeto, a descrição é escondida -Quando esta proximo ao objeto ativa um outline demonstrando pode interagir
    objetos que estao setados como de inventario vão para o inventario dps que a descrição é fechada (falta estabelecer quais objetos terão no jogos para estabelecer os ids para fazer o inventario funcionar corretamente)

---
+++ Assets/Scripts/ClickableObject.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClickableObject : MonoBehaviour {
+
+    public string descricao;
+    private bool clicado;
+    public bool ativado = false;
+    public bool pertoPlayer;
+    public bool objetoInventario = false;
+
+    public GameObject descricaoPanel;
+    public Text DescricaoText;
+
+    [Tooltip("Id do Objeto (Deve ser o mesmo id presente no objeto correspondente no script InventoryItens.cs)")]
+    public int id =1;
+
+    private Transform playerPosition;
+
+    [Tooltip("Distancia atual do player")]
+    private float distanciaPlayer;
+    [Tooltip("Distancia minima do personagem para que mostre ou esconda a descrição")]
+    public float distanciaMinima = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        if (gameObject.tag != "clickable")
+            gameObject.tag = "clickable";
+
+        DescricaoText.text = descricao;
+        if(descricaoPanel.gameObject.active == true)
+        {
+            descricaoPanel.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CloseToPlayer();
+
+        if(ativado == true)
+        {
+            ShowDescription();       
+        }
+        if(ativado == true && pertoPlayer == false)
+        {
+            HideDescription();
+        }
+
+    }
+
+    void CloseToPlayer()
+    {
+        distanciaPlayer = Vector2.Distance(playerPosition.position, transform.position);
+        if (distanciaPlayer < distanciaMinima)
+        {
+            pertoPlayer = true;
+            //GetComponent<SpriteRenderer>().color = Color.red;
+            GetComponent<SpriteOutline>().enabled = true;
+        }
+        else
+        {
+            pertoPlayer = false;
+            //GetComponent<SpriteRenderer>().color = Color.white;
+            GetComponent<SpriteOutline>().enabled = false;
+        }
+    }
+
+    public void ShowDescription()
+    {        
+        descricaoPanel.SetActive(true);
+        ativado = true;
+    }
+    public void HideDescription()
+    {
+        descricaoPanel.SetActive(false);
+        ativado = false;
+
+        if(clicado == true && objetoInventario == true)
+        {
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<Inventory>().AddToInventory(gameObject);
+            Destroy(gameObject);
+        }
+    }
+    
+    public int GetId()
+    {
+        return id;
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Sprite Clicked");
+
+        if (ativado == true) // se a descrição do objeto ja esta sendo mostrada e houve o clique no objeto novamente, a descrição é escondida
+        {
+            HideDescription();
+        }
+
+        else if (ativado == false) { // se a descricao esta escondida, ativa a variavel para mostrar a descricao
+            ativado = true;            
+        }
+        
+        clicado = true; // seta true em clicado para caso seja um objeto de inventario, ele va para la se ja foi clicado
+    }
+}
---
+++ Assets/Scripts/ClickableObjectsManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickableObjectsManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //RaycastHit2D hit = new RaycastHit2D();
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Debug.Log("Click esquerdo");
+        //    hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        //    if (hit.collider != null)
+        //    {
+        //        Debug.LogWarning("acertei");
+        //    }
+        //}
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.tag == "clickable")
+            {
+                Debug.LogWarning("Deucerto");
+            }
+        }
+
+        //RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        //Debug.Log("mouse pos " + mousePosition.x + " y " + mousePosition.y + " ");
+
+        //if (hit != null && hit.collider != null)
+        //{
+        //    Debug.Log("I'm hitting " + hit.collider.name);
+
+        //    if (Input.GetMouseButtonDown(0))
+        //    {
+        //        GameObject objetoSelecionado = hit.collider.gameObject;
+        //        if (objetoSelecionado.GetComponent<ClickableObject>().tag == "clickable")
+        //        {
+        //            Debug.Log("Bunda");
+        //        }
+        //    }
+
+        //}
+
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("Sprite Clicked");
+    }
+}
