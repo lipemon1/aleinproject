170095a359fa1570d6c0e91f62f4d123aed7427d
Jonnathan <jonnathan013@gmail.com>
2016-09-13 23:21:36 -0300

    Adicionado: - Algumas classes Util - Font que a gente mais usa do mobile - menuzinho simples

---
+++ Assets/Scripts/SphereGizmos.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereGizmos : MonoBehaviour {
+
+    public float gizmoSize = 1f;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, gizmoSize);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, gizmoSize);
+    }
+}
